c27101cbcfec2a5f99922045d3fa01dfa5a275bb
Eric <eric@system76-pc.localdomain>
2021-09-20 10:37:10 -0500

    add copyright and license

--- melMain.java
+++ melMain.java
@@ -1,3 +1,20 @@
+/*
+  The MEL programming language
+  Copyright 2015 Eric J. Deiman
+
+  This file is part of the MEL programming language.
+  The MEL programming language is free software: you can redistribute it
+  and/ormodify it under the terms of the GNU General Public License as published by the
+  Free Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  The MEL programming language is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with the
+  MEL programming language. If not, see <https://www.gnu.org/licenses/>
+*/
+
 import melParse.melLexer;
 import melParse.melParser;
 import melPrettyPrinter.melLispPrinter;
